bff43b06fd4a7fd07d21e5595b973a19ad45d130
Henrique Dourado Domingos <125815196+henriquedouradoo@users.noreply.github.com>
2025-10-02 23:20:07 -0300

    add: criando mais tabelas do script

--- script_cyberbeef.sql
+++ script_cyberbeef.sql
@@ -41,7 +41,64 @@ constraint fkPermissaoUsuario
 foreign key (permissaoUsuario) references permissaoUsuario(idPermissaoUsuario)
 );
 
-select * from usuario;
-select * from permissaoUsuario;
-select * from empresa;
-select * from contato;
+create table setor (
+idSetor int primary key auto_increment,
+tokenEmpresa int,
+nomeSetor varchar(45),
+descricao varchar(255),
+constraint fkSetorEmpresa
+foreign key (tokenEmpresa) references empresa(tokenEmpresa)
+);
+
+create table maquina (
+idMaquina int primary key auto_increment,
+macAddress varchar(45),
+ip varchar(45),
+hostname varchar(255),
+sistemaOperacional varchar(45)
+);
+
+create table setorMaquina (
+idSetor int,
+tokenEmpresa int,
+idMaquina int,
+status tinyint,
+responsavel varchar(45),
+dataVinculacao datetime,
+constraint fkSetorMaquina
+foreign key (idSetor) references setor(idSetor),
+foreign key (tokenEmpresa) references empresa(tokenEmpresa),
+foreign key (idMaquina) references maquina(idMaquina),
+primary key (idSetor, tokenEmpresa, idMaquina)
+);
+
+create table componente (
+idComponente int primary key auto_increment,
+idMaquina int,
+nomeComponente varchar(45),
+unidadeMedida varchar(45),
+constraint fkComponenteMaquina
+foreign key (idMaquina) references maquina(idMaquina)
+);
+
+create table nucleoCpu (
+idNucleo int primary key auto_increment,
+idComponente int,
+idMaquina int,
+constraint fkNucleoComponente
+foreign key (idComponente) references componente(idComponente),
+foreign key (idMaquina) references maquina(idMaquina)
+);
+
+create table Leitura (
+idLeitura int primary key auto_increment,
+idComponente int,
+idMaquina int,
+dado float,
+dthCaptura datetime,
+idNucleo int,
+constraint fkLeituraComponente
+foreign key (idComponente) references componente(idComponente),
+foreign key (idMaquina) references maquina(idMaquina),
+foreign key (idNucleo) references nucleoCpu(idNucleo)
+);
